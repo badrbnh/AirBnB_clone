a41c3e3252c1124cab59d7d1e22b332024d3124b
badrbnh <badrechahide@gmail.com>
2023-08-09 17:05:18 +0100

    BaseModel V2

--- models/base_model.py
+++ models/base_model.py
@@ -2,24 +2,51 @@
 
 from datetime import datetime
 import uuid
+
+
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """
+        Constructor for BaseModel class. Initializes instance
+        attributes based on provided
+        keyword arguments or generates default values for id, created_at,
+        and updated_at.
+        """
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            self.created_at = datetime.strptime(self.created_at, time_format)
+            self.updated_at = datetime.strptime(self.updated_at, time_format)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
+        """
+        Returns a string representation of the instance,
+        including its class name, id, and attributes.
+        """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
+        """
+        Updates the 'updated_at' attribute to the current datetime.
+        """
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
+        """
+        Converts the instance attributes into a dictionary representation
+        with 'simple object type'.
+        """
         dict = {}
-        dict['__class__'] = self.__class__.__name__
+        dict["__class__"] = self.__class__.__name__
         for key, value in self.__dict__.items():
             dict[key] = value
-            if key == 'created_at' or key == 'updated_at':
+            if key == "created_at" or key == "updated_at":
                 dict[key] = value.isoformat()
 
         return dict
