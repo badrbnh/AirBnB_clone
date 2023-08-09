de5475e8e8e50fbdac6258c3ef612b2e9d8b1635
NourMellal <mellal.nour9@gmail.com>
2023-08-09 15:43:10 +0100

    push: Creat BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,18 @@ import uuid
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        tFormat = "%Y-%m-%dT%H:%M:%S.%f"
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created_at
+        else:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] == datetime.datetime.strptime(v, tFormat)
+                else:
+                    self.__dict__[k] == v
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
