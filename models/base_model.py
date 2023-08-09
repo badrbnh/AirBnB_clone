222eb2f07df3b787a6124476a98e67be491fb13c
NourMellal <mellal.nour9@gmail.com>
2023-08-09 14:55:00 +0100

    Push: Pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,8 @@
 
 from datetime import datetime
 import uuid
+
+
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
@@ -10,16 +12,16 @@ class BaseModel:
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
         dict = {}
-        dict['__class__'] = self.__class__.__name__
+        dict["__class__"] = self.__class__.__name__
         for key, value in self.__dict__.items():
             dict[key] = value
-            if key == 'created_at' or key == 'updated_at':
+            if key == "created_at" or key == "updated_at":
                 dict[key] = value.isoformat()
 
         return dict
