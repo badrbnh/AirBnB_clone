e3162e02e62dc1e965c05451efbf4e37097afbc4
NourMellal <mellal.nour9@gmail.com>
2023-08-10 15:25:10 +0100

    File storage: Fix and implement when a JSON file is already exist

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,19 +12,28 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        self.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
+        self.__objects[obj.__class__.__name__ + "." + obj.id] = obj
 
     def save(self):
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, "r") as f:
+                exist = json.load(f)
+        else:
+            exist = {}
+
+        exist.update({k: v.to_dict() for k, v in self.__objects.items()})
+
         with open(self.__file_path, "w") as f:
-            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
+            json.dump(exist, f)
 
     def reload(self):
         from ..base_model import BaseModel
+
         file_path = self.__file_path
         if os.path.exists(file_path):
             with open(file_path, "r") as f:
                 objdict = json.load(f)
-                for k, v in objdict.values():
+                for k, v in objdict.items():
                     c_name = k.split('.')[1]
                     if c_name == 'BaseModel':
                         inst = BaseModel(**v)
