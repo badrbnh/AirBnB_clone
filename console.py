03223a8a64aae501a04aa5b9a33f6b140e0956bc
NourMellal <mellal.nour9@gmail.com>
2023-08-11 19:57:07 +0100

    console.py: implement parsing method and do_create command

---
+++ console.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+import cmd
+import re
+from shlex import split
+from models import storage
+from models.base_model import BaseModel
+
+
+def parsing(arg):
+    brack = re.search(r"\[(.*?)\]", arg)
+    c_braces = re.search(r"\{(.*?)\}", arg)
+
+    if c_braces is None:
+        if brack is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lex = split(arg[: brack.span()[0]])
+            reslist = [i.strip(",") for i in lex]
+            reslist.append(brack.group())
+            return reslist
+    else:
+        lex = split(arg[: c_braces.span()[0]])
+        reslist = [i.strip(",") for i in lex]
+        reslist.append(c_braces.group())
+        return reslist
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand class is a simple command-line interpreter that inherits
+    from cmd.Cmd. It provides a basic shell for interacting with the program.
+    """
+
+    prompt = "(hbnb) "
+    _classes = {
+        "BaseModel": BaseModel,
+    }
+
+    def emptyline(self):
+        """
+        Handles an empty line.
+        This method is called when the user enters an empty line.
+        Returns: True to continue the command loop.
+        """
+        pass
+
+    def do_EOF(self, args):
+        """
+        Handles the End of File (EOF) command.
+        This method is called when Ctrl+D is pressed.
+        Returns: True to exit the command loop.
+        """
+        print(" ")
+        return True
+
+    def do_quit(self, args):
+        """
+        Handles the 'quit' command.
+        This method is called when the user enters 'quit' in the shell.
+        Returns: True to exit the command loop.
+        """
+        return True
+
+    def do_create(self, args):
+        arg_list = parsing(args)
+        if len(arg_list) == 0:
+            print("** class name missing **")
+        elif arg_list[0] not in HBNBCommand._classes:
+            print("** class doesn't exist **")
+        else:
+            new_inst = HBNBCommand._classes[arg_list[0]]()
+            new_inst.save()
+            print(new_inst.id)
+
+
+if __name__ == "__main__":
+    """
+    This block of code is executed only when the script is run directly,
+    not when it's imported as a module.
+    Creates an instance of HBNBCommand and starts the command loop.
+    """
+    HBNBCommand().cmdloop()
