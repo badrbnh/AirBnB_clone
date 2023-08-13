bb608066b7a6458ec69381ebf61d789ba5c2dbe9
badrbnh <badrechahide@gmail.com>
2023-08-13 22:38:57 +0100

    Unittest FileStorage 0.3

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""Unittest for the FileStorage class"""
 import unittest
 import json
 import os
@@ -7,93 +9,63 @@ from models import storage
 
 
 class TestFileStorage(unittest.TestCase):
+    """Test cases for all methods in the FileStorage class"""
 
-    @classmethod
-    def setUpClass(cls):
-        # Initialize FileStorage and BaseModel instances
-        storage._FileStorage__objects = {}
-        cls.file_path = storage._FileStorage__file_path
-        cls.instance = BaseModel()
-        cls.keyname = f"{cls.instance.__class__.__name__}.{cls.instance.id}"
+    def setUp(self):
+        """Setting up the test cases"""
+        self.file_path = storage._FileStorage__file_path
+        self.instance = BaseModel()
+        self._objs = storage._FileStorage__objects
+        self.keyname = "BaseModel."+self.instance.id
 
-    @classmethod
-    def tearDownClass(cls):
-        # Clean up by removing the test file
+    def tearDown(self):
+        """Cleaning the file path"""
         try:
-            os.remove(cls.file_path)
+            os.remove(self.file_path)
         except FileNotFoundError:
             pass
 
-    def setUp(self):
-        # Reset objects dictionary before each test
-        storage._FileStorage__objects = {}
-
     def test_all_method(self):
-        storage._FileStorage__objects[self.keyname] = self.instance
+        """Test the all() method"""
         result = storage.all()
-        self.assertEqual(result, storage._FileStorage__objects)
+        self.assertEqual(result, self._objs)
 
     def test_new_method(self):
+        """Test the new()) method"""
         storage.new(self.instance)
-        self.assertIn(self.keyname, storage._FileStorage__objects)
+        key = f"{self.instance.__class__.__name__}.{self.instance.id}"
+        self.assertIn(key, self._objs)
 
     def test_save_method(self):
+        """Test the save() method"""
         my_model = BaseModel()
         my_model.name = "My_First_Model"
         my_model.my_number = 89
-        storage.new(my_model)
         storage.save()
-
         with open(self.file_path, "r") as data_file:
             saved_data = json.load(data_file)
 
-        self.assertEqual(saved_data[self.keyname], my_model.to_dict())
+        expected_data = {}
+        for key, value in self._objs.items():
+            expected_data[key] = value.to_dict()
+
+        self.assertEqual(saved_data, expected_data)
 
     def test_reload_method(self):
+        """save the reload() method"""
         my_model = BaseModel()
         my_model.name = "My_First_Model"
         my_model.my_number = 89
-        storage.new(my_model)
         storage.save()
-
         with open(self.file_path, 'r') as file:
             saved_data = json.load(file)
-
         storage.reload()
 
         with open(self.file_path, 'r') as file:
             reloaded_data = json.load(file)
 
+        self._objs = {}
         self.assertEqual(reloaded_data[self.keyname], saved_data[self.keyname])
-
-    def test_delete_method(self):
-        my_model = BaseModel()
-        my_model.name = "Delete_Test_Model"
-        storage.new(my_model)
-        key_to_delete = f"{my_model.__class__.__name__}.{my_model.id}"
-        storage.delete(my_model)
-        self.assertNotIn(key_to_delete, storage._FileStorage__objects)
-
-    def test_get_method(self):
-        my_model = BaseModel()
-        my_model.name = "Get_Test_Model"
-        storage.new(my_model)
-        key_to_get = f"{my_model.__class__.__name__}.{my_model.id}"
-        retrieved_model = storage.get(BaseModel, my_model.id)
-        self.assertEqual(retrieved_model, my_model)
-
-    def test_count_method(self):
-        initial_count = len(storage.all())
-        my_model = BaseModel()
-        storage.new(my_model)
-        new_count = storage.count()
-        self.assertEqual(new_count, initial_count + 1)
-
-
-if __name__ == "__main__":
-    unittest.main()
-
-
 
 if __name__ == "__main__":
     unittest.main()
