dbdb76808197e44a52eba80ec3816512e6877c57
badrbnh <badrechahide@gmail.com>
2023-08-13 22:06:56 +0100

    check 0.1

--- models/place.py
+++ models/place.py
@@ -24,10 +24,10 @@ class Place(BaseModel):
     user_id = ""
     name = ""
     description = ""
-    number_rooms = int
-    number_bathrooms = int
-    max_guest = int
-    price_by_night = int
-    latitude = float
-    longitude = float
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
     amenity_ids = []
