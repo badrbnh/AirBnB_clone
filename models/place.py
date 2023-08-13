3d377118080d2beaa12d2fd6fefbf7fb4ccc2a08
badrbnh <badrechahide@gmail.com>
2023-08-13 18:53:06 +0100

    Test more classes

--- models/place.py
+++ models/place.py
@@ -23,10 +23,10 @@ class Place(BaseModel):
     city_id = ""
     user_id = ""
     description = ""
-    number_rooms = int
-    number_bathrooms = int
-    max_guest = int
-    price_by_night = int
-    latitude = float
-    longitude = float
-    amenity_ids = [""]
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
